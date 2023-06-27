f2e28079dc3f6861a926b8afb15aad171ae64a4c
hi-unc1e <hi-unc1e@users.noreply.github.com>
2023-06-27 16:40:41 +0000

    deploy: e1283fa6a1b159b9edbbacfe31223d85c8c0992e

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# build sub-module
-cd sub_cheatsheets && npm install && npm run build  && cd ..
--- sub_cheatsheets/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: {
-        'postcss-import': {path: 'themes/coo/source/css'},
-        'tailwindcss': {config: './tailwind.config.js'},
-        'autoprefixer': {},
-        'postcss-minify':{},
-    }
-};
